b0c9a6324e3dd25fdc95b47d100283d63fd4a99d
ihentx <ihengeo@gmail.com>
2023-01-09 20:48:38 +0300

    a

--- test3.py
+++ test3.py
@@ -2,7 +2,6 @@ import pygame
 
 
 def chess(screen, n):
-    screen.fill((255, 255, 255))
     if n % 2 == 0:
         flag = 1
     else:
@@ -18,24 +17,20 @@ def chess(screen, n):
             y += a // n * 2
         x += a // n
         flag = 1 - flag
- 
- 
+
+
 if __name__ == '__main__':
-    pygame.init()
-    try:
-        a, n = input().split()
-        if int(a) != float(a) or int(n) != float(n) or int(a) % int(n) != 0:
-            print('Неправильный формат ввода')
-        else:
-            a = int(a)
-            n = int(n)
-            size = width, height = a, a
-            screen = pygame.display.set_mode(size)
-            chess(screen, n)
-            pygame.display.flip()
-            while pygame.event.wait().type != pygame.QUIT:
-                pass
-            pygame.quit()
-    except Exception:
+    a, n = input().split()
+    if type(a) == float or type(n) == float or int(a) % int(n) != 0:
         print('Неправильный формат ввода')
- 
+    else:
+        a, n = int(a), int(n)
+        pygame.init()
+        size = width, height = a, a
+        screen = pygame.display.set_mode(size)
+        chess(screen, n)
+        pygame.display.flip()
+        while pygame.event.wait().type != pygame.QUIT:
+            pass
+        pygame.quit()
+    
